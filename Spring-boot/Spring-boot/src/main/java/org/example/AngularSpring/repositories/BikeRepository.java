9f323cefd6554ae89d84a55314c056b9251ec87c
Beerelli <sanjaykumar.beerelli@accenture.com>
2020-03-10 10:35:48 +0100

    "Deleted Spring boot code"

--- Spring-boot/Spring-boot/src/main/java/org/example/AngularSpring/repositories/BikeRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.AngularSpring.repositories;
-
-import org.example.AngularSpring.models.Bike;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.math.BigInteger;
-
-public interface BikeRepository extends JpaRepository<Bike, BigInteger> {
-}
